2550035a24b7687081d4d102a7f2106746867686
Peter Lowe <peter.lowe@itcarlow.ie>
2020-10-22 12:25:06 +0100

    random seed

--- Bucket of Dice/main.cpp
+++ Bucket of Dice/main.cpp
@@ -6,10 +6,14 @@
 // session 1 start 12:00
 
 #include <iostream> // cout & cin
+#include <cstdlib> // srand & rand
+#include <ctime> // time
 
 int main()
 {
 	int diceFaces = 0;
+	
+	std::srand(static_cast<unsigned>(std::time(nullptr)));
 	std::cout << "How many faces on the dice? ";
 	std::cin >> diceFaces;
 	return 1;
